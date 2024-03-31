44a87e2bd35680a00c51b8f45082b596f25b332f
ManiTyagi <manityagi1919@gmail.com>
2024-03-31 23:49:16 +0530

    Added cloudinary media uploader

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -34,6 +34,7 @@ import { startTransition, useEffect, useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import { useRouter } from "next/router";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
     title: z.string(),
@@ -267,6 +268,10 @@ const TransformationForm = ({
                         )}
                     </div>
                 )}
+
+                <div className="media-uploader-field">
+                    <CustomField control={form.control} name="publicId" className="flex size-full flex-col" render={({ field }) => (<MediaUploader onValueChange={field.onChange} setImage={setImage} publicId={field.value} image= {image} type={type} />)}/>
+                </div>
 
                 <div className="flex flex-col gap-4">
                     <Button
