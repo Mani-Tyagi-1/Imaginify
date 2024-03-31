65ab322d625c66f35c207844531ccc034a4e9925
Prashant Sagar Shakya <prashant.sagar.shakya@gmail.com>
2024-03-31 17:55:54 +0530

    Added Server Actions and WebHooks

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="root">
             <Sidebar />
-            <MobileNav/>
+            <MobileNav />
             <div className="root-container">
                 <div className="wrapper">{children}</div>
             </div>
--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,18 @@
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
 import React from "react";
 
-const AddTransformationTypePage = () => {
-    return <div>AddTransformationTypePage</div>;
+const AddTransformationTypePage = async ({
+    params: { type },
+}: SearchParamProps) => {
+    const transformation = transformationTypes[type];
+     
+    return (
+        <Header
+            title={transformation.title}
+            subtitle={transformation.subTitle}
+        />
+    );
 };
 
 export default AddTransformationTypePage;
