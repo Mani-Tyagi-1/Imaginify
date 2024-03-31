4d81e6c470e4281cad8af6a7f4a985e68c2362b1
Prashant Sagar Shakya <prashant.sagar.shakya@gmail.com>
2024-03-31 16:42:00 +0530

    Imaginify Created

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TransformationsPage = ({}) => {
+    return <div>TransformationsPage</div>;
+};
+
+export default TransformationsPage;
+
+//localhost:3000/transformations
+//localhost:3000/transformations/[id]
