cd472e38055bbb145d44c145f260cade15dc1825
Prashant Sagar Shakya <prashant.sagar.shakya@gmail.com>
2024-03-31 21:11:21 +0530

    Added Image Form (React Hook Form + Zod Validation)

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,24 +7,24 @@ import { twMerge } from "tailwind-merge";
 import { aspectRatioOptions } from "@/constants";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 // ERROR HANDLER
 export const handleError = (error: unknown) => {
-  if (error instanceof Error) {
-    // This is a native JavaScript error (e.g., TypeError, RangeError)
-    console.error(error.message);
-    throw new Error(`Error: ${error.message}`);
-  } else if (typeof error === "string") {
-    // This is a string error message
-    console.error(error);
-    throw new Error(`Error: ${error}`);
-  } else {
-    // This is an unknown type of error
-    console.error(error);
-    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
-  }
+    if (error instanceof Error) {
+        // This is a native JavaScript error (e.g., TypeError, RangeError)
+        console.error(error.message);
+        throw new Error(`Error: ${error.message}`);
+    } else if (typeof error === "string") {
+        // This is a string error message
+        console.error(error);
+        throw new Error(`Error: ${error}`);
+    } else {
+        // This is an unknown type of error
+        console.error(error);
+        throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+    }
 };
 
 // PLACEHOLDER LOADER - while image is transforming
@@ -43,115 +43,116 @@ const shimmer = (w: number, h: number) => `
 </svg>`;
 
 const toBase64 = (str: string) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
+    typeof window === "undefined"
+        ? Buffer.from(str).toString("base64")
+        : window.btoa(str);
 
 export const dataUrl = `data:image/svg+xml;base64,${toBase64(
-  shimmer(1000, 1000)
+    shimmer(1000, 1000)
 )}`;
 // ==== End
 
 // FORM URL QUERY
 export const formUrlQuery = ({
-  searchParams,
-  key,
-  value,
+    searchParams,
+    key,
+    value,
 }: FormUrlQueryParams) => {
-  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+    const params = { ...qs.parse(searchParams.toString()), [key]: value };
 
-  return `${window.location.pathname}?${qs.stringify(params, {
-    skipNulls: true,
-  })}`;
+    return `${window.location.pathname}?${qs.stringify(params, {
+        skipNulls: true,
+    })}`;
 };
 
 // REMOVE KEY FROM QUERY
 export function removeKeysFromQuery({
-  searchParams,
-  keysToRemove,
+    searchParams,
+    keysToRemove,
 }: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(searchParams);
+    const currentUrl = qs.parse(searchParams);
 
-  keysToRemove.forEach((key) => {
-    delete currentUrl[key];
-  });
+    keysToRemove.forEach((key) => {
+        delete currentUrl[key];
+    });
 
-  // Remove null or undefined values
-  Object.keys(currentUrl).forEach(
-    (key) => currentUrl[key] == null && delete currentUrl[key]
-  );
+    // Remove null or undefined values
+    Object.keys(currentUrl).forEach(
+        (key) => currentUrl[key] == null && delete currentUrl[key]
+    );
 
-  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+    return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
 }
 
 // DEBOUNCE
 export const debounce = (func: (...args: any[]) => void, delay: number) => {
-  let timeoutId: NodeJS.Timeout | null;
-  return (...args: any[]) => {
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
-  };
+    let timeoutId: NodeJS.Timeout | null;
+    return (...args: any[]) => {
+        if (timeoutId) clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => func.apply(null, args), delay);
+    };
 };
 
 // GE IMAGE SIZE
 export type AspectRatioKey = keyof typeof aspectRatioOptions;
 export const getImageSize = (
-  type: string,
-  image: any,
-  dimension: "width" | "height"
+    type: string,
+    image: any,
+    dimension: "width" | "height"
 ): number => {
-  if (type === "fill") {
-    return (
-      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
-      1000
-    );
-  }
-  return image?.[dimension] || 1000;
+    if (type === "fill") {
+        return (
+            aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[
+                dimension
+            ] || 1000
+        );
+    }
+    return image?.[dimension] || 1000;
 };
 
 // DOWNLOAD IMAGE
 export const download = (url: string, filename: string) => {
-  if (!url) {
-    throw new Error("Resource URL not provided! You need to provide one");
-  }
+    if (!url) {
+        throw new Error("Resource URL not provided! You need to provide one");
+    }
 
-  fetch(url)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const blobURL = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = blobURL;
+    fetch(url)
+        .then((response) => response.blob())
+        .then((blob) => {
+            const blobURL = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = blobURL;
 
-      if (filename && filename.length)
-        a.download = `${filename.replace(" ", "_")}.png`;
-      document.body.appendChild(a);
-      a.click();
-    })
-    .catch((error) => console.log({ error }));
+            if (filename && filename.length)
+                a.download = `${filename.replace(" ", "_")}.png`;
+            document.body.appendChild(a);
+            a.click();
+        })
+        .catch((error) => console.log({ error }));
 };
 
 // DEEP MERGE OBJECTS
 export const deepMergeObjects = (obj1: any, obj2: any) => {
-  if (obj2 === null || obj2 === undefined) {
-    return obj1;
-  }
-
-  let output = { ...obj2 };
-
-  for (let key in obj1) {
-    if (obj1.hasOwnProperty(key)) {
-      if (
-        obj1[key] &&
-        typeof obj1[key] === "object" &&
-        obj2[key] &&
-        typeof obj2[key] === "object"
-      ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
-      } else {
-        output[key] = obj1[key];
-      }
+    if (obj2 === null || obj2 === undefined) {
+        return obj1;
     }
-  }
 
-  return output;
+    let output = { ...obj2 };
+
+    for (let key in obj1) {
+        if (obj1.hasOwnProperty(key)) {
+            if (
+                obj1[key] &&
+                typeof obj1[key] === "object" &&
+                obj2[key] &&
+                typeof obj2[key] === "object"
+            ) {
+                output[key] = deepMergeObjects(obj1[key], obj2[key]);
+            } else {
+                output[key] = obj1[key];
+            }
+        }
+    }
+
+    return output;
 };
